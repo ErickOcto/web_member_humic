b3a41f3a740476d52c6096829409b70d8cf57d7d
ErickOcto <128987328+ErickOcto@users.noreply.github.com>
2024-10-21 14:34:50 +0700

    improve landing index

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,73 @@
 @extends('layouts.landing')
 
+@push('css_scripts')
+<style>
+    body{
+        max-height: 100vh !important;
+
+        
+    }
+</style>
+@endpush
+
 @section('landing-content')
 
+<section class="content">
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-md-6 mt-5">
+                <h3 class="text-danger text-start"><b>Welcome to the HUMIC Engineering Member Portal!</b></h3>
 
-            <section class="content">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-12 col-md-6 mt-5">
-                            <h3 class="text-danger text-start"><b>Welcome to the HUMIC Engineering Member Portal!</b></h3>
-
-                            {{-- FORM --}}
-                            <form method="POST" action="{{ route('login') }}">
-                            @csrf
-                              <div class="my-3 text-start">
-                                <label for="exampleInputEmail1" class="form-label">Username</label>
-                                <input type="text" class="form-control @error('username') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp" name="username">
-                                @error('username')
-                                    <b>Ada yang salah dengan username</b>
-                                @enderror
-                              </div>
-                              <div class="mb-3 text-start">
-                                <label for="exampleInputPassword1" class="form-label @error('password') is-invalid @enderror">Password</label>
-                                <input type="password" class="form-control" id="exampleInputPassword1" name="password">
-                                @error('password')
-                                    <b>Ada yang salah dengan password</b>@enderror
-                              </div>
-                              <div class="d-grid">
-                                <button type="submit" class="btn btn-danger">LOGIN</button>
-                              </div>
-                            </form>
-
-                        </div>
-                        <div class="col-12 col-md-6">
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/Adiwijaya.png" alt="Avatar 1">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/image-10-1.png" alt="Avatar 2">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/lastri-1.jpg" alt="Avatar 3">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/435A4423-1.jpg" alt="Avatar 4">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/amilaa-1.png" alt="Avatar 5">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/profile_satria.jpg" alt="Avatar 6">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/Putu-Harry-Gunawan.jpeg" alt="Avatar 6">
-                            </div>
-                        </div>
+                {{-- FORM --}}
+                <form method="POST" action="{{ route('login') }}">
+                @csrf
+                  <div class="my-3 text-start">
+                    <label for="exampleInputEmail1" class="form-label">Username</label>
+                    <input type="text" class="form-control @error('username') is-invalid @enderror" id="exampleInputEmail1" aria-describedby="emailHelp" name="username">
+                    @error('username')
+                        <b>Ada yang salah dengan username</b>
+                    @enderror
+                  </div>
+                  <div class="mb-3 text-start">
+                    <label for="exampleInputPassword1" class="form-label @error('password') is-invalid @enderror">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" name="password">
+                    @error('password')
+                        <b>Ada yang salah dengan password</b>@enderror
+                  </div>
+                  <div class="d-grid">
+                    <button type="submit" class="btn btn-danger">LOGIN</button>
+                  </div>
+                </form>
+            </div>
+            <div class="col-12 col-md-6">
+                <div class="avatar-grid">
+                    <div class="avatar">
+                        <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/Adiwijaya.png" alt="Avatar 1">
+                    </div>
+                    <div class="avatar">
+                        <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/image-10-1.png" alt="Avatar 2">
+                    </div>
+                    <div class="avatar">
+                        <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/lastri-1.jpg" alt="Avatar 3">
+                    </div>
+                    <div class="avatar">
+                        <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/435A4423-1.jpg" alt="Avatar 4">
+                    </div>
+                    <div class="avatar">
+                        <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/amilaa-1.png" alt="Avatar 5">
+                    </div>
+                    <div class="avatar">
+                        <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/profile_satria.jpg" alt="Avatar 6">
+                    </div>
+                    <div class="avatar">
+                        <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/Putu-Harry-Gunawan.jpeg" alt="Avatar 7">
                     </div>
                 </div>
-            </section>
+            </div>
+        </div>
+    </div>
+</section>
+
 
 
 @endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,45 @@
 @extends('layouts.landing')
 
+@push('css_scripts')
+    <style>
+        .stat-box {
+            background-color: #fff;
+            border-radius: 15px;
+            box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+            text-align: center;
+            padding: 20px;
+        }
+
+        .stat-box h4 {
+            margin-top: 10px;
+            color: #ff6f6f;
+        }
+
+        .stat-box h2 {
+            font-weight: bold;
+            color: #333;
+        }
+
+        .chart-container {
+            background-color: #fff;
+            padding: 20px;
+            border-radius: 15px;
+            box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+        }
+
+        .chart-container canvas {
+            max-height: 400px;
+        }
+
+        .title-text {
+            text-align: center;
+            color: #e74c3c;
+            font-weight: bold;
+            margin-bottom: 30px;
+        }
+    </style>
+@endpush
+
 @section('landing-content')
 
             <section class="content">
@@ -14,35 +54,160 @@
                                 technology closer to real life. Together, let's build a healthier and more
                                 prosperous future!
                             </p>
-                            <p class="text-start">
-                                <a href="#" class="btn btn-danger">Button</a>
-                            </p>
                         </div>
                         <div class="col-12 col-md-6">
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/Adiwijaya.png" alt="Avatar 1">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/image-10-1.png" alt="Avatar 2">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/lastri-1.jpg" alt="Avatar 3">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/435A4423-1.jpg" alt="Avatar 4">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/amilaa-1.png" alt="Avatar 5">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/profile_satria.jpg" alt="Avatar 6">
-                            </div>
-                            <div class="avatar">
-                                <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/Putu-Harry-Gunawan.jpeg" alt="Avatar 6">
+                            <div class="avatar-grid">
+                                <div class="avatar">
+                                    <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/Adiwijaya.png" alt="Avatar 1">
+                                </div>
+                                <div class="avatar">
+                                    <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/image-10-1.png" alt="Avatar 2">
+                                </div>
+                                <div class="avatar">
+                                    <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/lastri-1.jpg" alt="Avatar 3">
+                                </div>
+                                <div class="avatar">
+                                    <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/435A4423-1.jpg" alt="Avatar 4">
+                                </div>
+                                <div class="avatar">
+                                    <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2024/05/amilaa-1.png" alt="Avatar 5">
+                                </div>
+                                <div class="avatar">
+                                    <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/profile_satria.jpg" alt="Avatar 6">
+                                </div>
+                                <div class="avatar">
+                                    <img src="https://humic.telkomuniversity.ac.id/wp-content/uploads/2020/06/Putu-Harry-Gunawan.jpeg" alt="Avatar 7">
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
 
+<section class="container my-5">
+        <!-- Title Section -->
+        <h2 class="title-text">STATISTIC</h2>
+
+        <!-- Stat Boxes Section -->
+        <div class="row text-center">
+            <div class="col-12 col-md-4 text-center pb-5">
+                <div class="stat-box">
+                    <div class="text-center p-4 d-flex justify-content-center align-items-center" >
+                        <svg class="p-4" xmlns="http://www.w3.org/2000/svg" width="32" style="background: #ffe4e4; width:fit-content; height:fit-content; border-radius:50%;" height="32" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                    </div>
+                    <h4>Prodi</h4>
+                    <h2>58</h2>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-4 text-center pb-5">
+                <div class="stat-box">
+                    <div class="amba text-center p-4 d-flex justify-content-center align-items-center" >
+                        <svg class="p-4" xmlns="http://www.w3.org/2000/svg" width="32" style="background: #ffe4e4; width:fit-content; height:fit-content; border-radius:50%;" height="32" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
+                    </div>
+                    <h4>Fakultas</h4>
+                    <h2>10</h2>
+                </div>
+            </div>
+
+            <div class="col-12 col-md-4 text-center pb-5">
+                <div class="stat-box">
+                    <div class="text-center p-4 d-flex justify-content-center align-items-center" >
+                        <svg class="p-4" xmlns="http://www.w3.org/2000/svg" width="32" height="32" style="background: #ffe4e4; width:fit-content; height:fit-content; border-radius:50%;" viewBox="0 0 24 24" fill="none" stroke="red" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-monitor"><rect x="2" y="3" width="20" height="14" rx="2" ry="2"></rect><line x1="8" y1="21" x2="16" y2="21"></line><line x1="12" y1="17" x2="12" y2="21"></line></svg>
+                    </div>
+                    <h4>Cabang</h4>
+                    <h2>4</h2>
+                </div>
+            </div>
+        </div>
+
+        <!-- Chart Section -->
+        <div class="row mt-4">
+            <div class="col-12">
+                <div class="chart-container">
+                    <canvas id="myChart"></canvas>
+                </div>
+            </div>
+        </div>
+</section>
+
+    {{-- <div class="my-5">
+        r
+    </div> --}}
+
+    <section class="container my-5">
+        <h2 class="title-text">Project Gallery</h2>
+        <div class="row gy-5">
+            <div class="col-12 col-md-6">
+                <h3><b>Project Web Humic</b></h3>
+                <div class="gallery-card">
+                    <img class="img-fluid" src="{{ asset('assets/img/pg-1.png') }}" alt="pg-1">
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <h3><b>Project Web Humic 2</b></h3>
+                <div class="gallery-card">
+                    <img class="img-fluid" src="{{ asset('assets/img/pg-2.png') }}" alt="pg-2">
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <h3><b>Project Humic</b></h3>
+                <div class="gallery-card">
+                    <img class="img-fluid" src="{{ asset('assets/img/pg-3.png') }}" alt="pg-3">
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <h3><b>Project Media Humic</b></h3>
+                <div class="gallery-card">
+                    <img class="img-fluid" src="{{ asset('assets/img/pg-4.png') }}" alt="pg-4">
+                </div>
+            </div>
+            <div class="col-12 col-md-6">
+                <h3><b>Project LinkedIN</b></h3>
+                <div class="gallery-card">
+                    <img class="img-fluid" src="{{ asset('assets/img/pg-5.png') }}" alt="pg-5">
+                </div>
+            </div>
+        </div>
+    </section>
+
 @endsection
+
+@push('js_scripts')
+<script>
+        const ctx = document.getElementById('myChart').getContext('2d');
+        const myChart = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                labels: ['2024', '2025', '2026', '2027', '2028'],
+                datasets: [{
+                    label: 'Jumlah Member',
+                    data: [35, 40, 15, 15, 10],
+                    backgroundColor: [
+                        '#e74c3c', // Red bars
+                        '#ff7979', // Light red bars
+                        '#e74c3c',
+                        '#ff7979',
+                        '#e74c3c'
+                    ],
+                    borderColor: [
+                        '#e74c3c',
+                        '#ff7979',
+                        '#e74c3c',
+                        '#ff7979',
+                        '#e74c3c'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: true,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    </script>
+@endpush
